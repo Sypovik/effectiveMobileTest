ac3688af05cc37c124968fe3ee81fa0e0a5dee3a
sypovik <nekich0999@gmail.com>
2025-06-05 19:48:52 +0700

    Написал точку входа

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
-	"fmt"
-
-	"github.com/Sypovik/effectiveMobileTest/internal/config"
+	"github.com/Sypovik/effectiveMobileTest/internal/db"
+	"github.com/Sypovik/effectiveMobileTest/internal/handlers"
+	"github.com/Sypovik/effectiveMobileTest/internal/repository"
+	"github.com/Sypovik/effectiveMobileTest/internal/services"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	config := config.LoadConfig()
-	fmt.Println(config)
+	d := db.Init()
+	r := repository.NewPgPersonRepository(d)
+	s := services.NewPersonService(r)
+	router := gin.Default()
+	handlers.RegisterPersonRoutes(router, *s)
+	router.Run()
 }
